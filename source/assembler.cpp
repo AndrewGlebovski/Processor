0e579046286ffcccd85cd442145ce104beb171b0
Glebovski <andrewglebovski@gmail.com>
2022-10-09 21:08:08 +0300

    Add Program struct
    Добавил структура, которая содержит информацию о программе

--- source/assembler.cpp
+++ source/assembler.cpp
@@ -4,6 +4,13 @@
 #include "text.hpp"
 
 
+/// Contains information about byte code to execute
+typedef struct {
+    int *code = nullptr; ///< Operation code 
+    size_t count = 0; ///< Operation count
+} Program;
+
+
 int main() {
     int input = open("debug/source.txt", O_RDONLY);
 
@@ -18,6 +25,9 @@ int main() {
 
     for(int i = 0; i < text.size; i++)
         printf("[%.3i] %s\n", text.lines[i].len, text.lines[i].str);
+
+    Program program = {};
+    printf("%p %llu\n", program.code, program.count);
 
     printf("Assembler!\n");
 
