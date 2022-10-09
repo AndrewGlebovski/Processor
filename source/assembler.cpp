b0b355d6aab00b0f5620851e494c8fda0fa56e6b
Glebovski <andrewglebovski@gmail.com>
2022-10-09 20:53:32 +0300

    Add file opening
    Добавил открытие файла ввода на чтение

--- source/assembler.cpp
+++ source/assembler.cpp
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <io.h>
 #include "text.hpp"
 
 
 int main() {
+    int input = open("debug/source.txt", O_RDONLY);
+
+    if (input == -1) {
+        printf("Couldn't open file!\n");
+        return 1;
+    }
+
     printf("Assembler!\n");
 
     return 0;
